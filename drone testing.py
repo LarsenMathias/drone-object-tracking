3ed6f7c9de40fe8694fe3db4a6d45fe9e7404de0
LarsenMathias <88501525+LarsenMathias@users.noreply.github.com>
2022-04-21 21:09:14 +0530

    Update drone testing.py

--- drone testing.py
+++ drone testing.py
@@ -2,8 +2,8 @@ from djitellopy import Tello
 import cv2, math, time
 import numpy as np
 fbrange=[6200,6800]
-pid=[0.4,0.4,0]
-pError=0
+# pid=[0.4,0.4,0]
+# pError=int(0)
 w,h=360,240
 tello = Tello()
 tello.connect()
@@ -11,7 +11,7 @@ tello.streamon()
 frame_read = tello.get_frame_read()
 tello.takeoff()
 # tello.send_rc_control(0,0,25,0)
-# time.sleep(1.5)
+# time.sleep(1.8)
 def findFace(img):
     faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     imgGray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -31,54 +31,58 @@ def findFace(img):
         return img,[myFaceListc[i],myFaceListArea[i]]
     else:
         return img,[[0,0],0]
-def tracMe(info,w,pid,pError):
+def tracMe(info):
+    # pError=0
     fb=0
     area=info[1]
     x,y=info[0]
-    error=x-w//2
-    speed=pid[0]*error+pid[1]*(error-pError)
-    speed=int(np.clip(speed,-100,100))
-    if area>fbrange(0) and area<fbrange(1):
+    # error=x-w//2
+    # speed=pid[0]*error+pid[1]*(error-pError)
+    # speed=int(np.clip(speed,-100,100))
+    if area>fbrange[0] and area<fbrange[1]:
         fb=0
     elif area>fbrange[1]:
         fb=-20
-    elif area<fbrange[0] and area!=0:
+    else: 
         fb=20 
-    if x==0:
-        speed=0
-        error=0
-    print(speed,fb)
-    tello.send_rc_control(0,fb,0,speed)
-    return error
+    # if x==0:
+    #     speed=0
+    #     error=0
+    # if y==0:
+    #     speed=0
+    #     error=0
+    print(fb)
+    tello.send_rc_control(0,fb,0,0)
+    # return error
 
 while True:
     img = frame_read.frame
-    img=cv2.resize(img,(w,h))
+    # img=cv2.resize(img)
     img,info=findFace(img)
-    tracMe(info,w,pid,pError)
-    # print("Center ",info[0],"Area ",info[1])
+    findFace(img)
+    tracMe(info)
+    print("Area ",info[1])
     cv2.imshow("drone", img)
-    if cv2.waitKey(1) & 0xFF==ord("q"): 
-    # if key == '2': 
-    #     break
-    # elif key == ord('w'):
-    #     tello.move_forward(30)
-    # elif key == ord('s'):
-    #     tello.move_back(30)
-    # elif key == ord('a'):
-    #     tello.move_left(30)
-    # elif key == ord('d'):
-    #     tello.move_right(30)
-    # elif key == ord('e'):
-    #     tello.rotate_clockwise(30)
-    # elif key == ord('q'):
-    #     tello.rotate_counter_clockwise(30)
-    # elif key == ord('r'):
-    #     tello.move_up(30)
-    # elif key == ord('f'):
-    #     tello.move_down(30)
-        tello.land()
+    key = cv2.waitKey(1) & 0xff
+    if key == 27: # ESC
         break
+    elif key == ord('w'):
+        tello.move_forward(30)
+    elif key == ord('s'):
+        tello.move_back(30)
+    elif key == ord('a'):
+        tello.move_left(30)
+    elif key == ord('d'):
+        tello.move_right(30)
+    elif key == ord('e'):
+        tello.rotate_clockwise(30)
+    elif key == ord('q'):
+        tello.rotate_counter_clockwise(30)
+    elif key == ord('r'):
+        tello.move_up(30)
+    elif key == ord('f'):
+        tello.move_down(30)
+
 
 
 
